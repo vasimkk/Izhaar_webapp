1ab2d1dd59cbc3ca48a116f3547132950c39ab35
vasim kureshi <kkvasim417@gamail.com>
2025-12-26 15:40:36 +0530

    new project setups

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
