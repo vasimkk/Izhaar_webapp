f8cdcd666cd028ef75ab9acbaa9de5d4e9c6430f
vasim kureshi <kkvasim417@gamail.com>
2026-01-13 17:36:44 +0530

    updated code

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -2,7 +2,7 @@
 const BASE_URL = import.meta.env.VITE_API_URL || 
                  import.meta.env.EXPO_PUBLIC_API_URL || 
                  (import.meta.env.MODE === 'production' 
-                   ? 'https://izhaarlove.com' 
+                   ? 'https://api.izhaarlove.com' 
                    : 'http://192.168.0.124:5000');
 
 export { BASE_URL };
